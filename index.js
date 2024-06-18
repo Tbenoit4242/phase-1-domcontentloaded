79ddf683b0077afab908d9fe5e171c7b76875713
Trevor Benoit <Trevorb042@gmail.com>
2024-06-18 13:59:42 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+ 
+    const paragraph = document.getElementById("text");
+
+    // Replace the text content of the paragraph element
+    paragraph.textContent = "This is really cool!";
+  });
+  
